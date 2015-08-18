d90eddb4e9f2dc3bfb28eae141d6477467b57a8e
Brian Kelly <pair+bkelly@pivotal.io>
2015-08-18 13:20:14 -0400

    Adds Bootstrap via WebJars

---
+++ src/test/java/demo/WebConfig.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
