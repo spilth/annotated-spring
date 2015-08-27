2163fe4676ef223060d4e587d9a144c38ad884bc
Brian Kelly <polymonic@gmail.com>
2015-08-27 10:48:52 -0400

    Import example episode into development environment.

---
+++ src/main/resources/import-development.sql
@@ -0,0 +1,17 @@
+INSERT INTO episode (
+  id,
+  title,
+  summary,
+  notes,
+  duration,
+  youtube_id,
+  sourcecode_url
+) VALUES (
+  1,
+  'Spring Boot QuickStart',
+  'Starting a Spring Boot project using the Spring Boot CLI',
+  'Episode Notes',
+  3,
+  '7cOVaxlxA5k',
+  'https://github.com/AnnotatedSpring/annotated-spring'
+);
